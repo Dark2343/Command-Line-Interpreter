342f246969985048113eaf37810418af4441c77a
Dark <mohamed2003elashmawy@gmail.com>
2023-11-05 05:55:04 +0200

    Split the program into 3 files

---
+++ CLI.java
@@ -0,0 +1,10 @@
+public class CLI {
+    // Main method
+    public static void main(String[] args)
+    {
+        Terminal terminal = new Terminal();
+        while (true) {
+            terminal.execute();
+        }
+    }
+}
